200a9008df5706b5c584918a95a572ef9e3249db
Osvaldo Angel Hernandez Gutierrez <osvaldoangelhernandezgutierrez@Mini-de-Osvaldo.domain.name>
2025-04-07 11:20:45 -0600

    prueba

--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,4 @@ void main() async{
 }
 
   print('=========ENVIROMENT SELECTED: ');                                         
+  print('=========ENVIROMENT SELECTED: ');                                         
